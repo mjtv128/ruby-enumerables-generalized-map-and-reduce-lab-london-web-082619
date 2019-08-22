0c71e29dcf40edfb77be2631436aa128d61b8a3c
jocular-source-7570 <mjtvan@gmail.com>
2019-08-22 18:12:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,16 @@
 # Your Code Here
 def my_own_map(source_array)
   source_array.map{|x| x * -1}
+end 
+
+def my_own_map(source_array)
   source_array.map{|x| x}
+end 
+
+def my_own_map(source_array)
   source_array.map{|x| x*2}
+end 
+
+def my_own_map(source_array)
   source_array.map{|x| x **2}
 end 
