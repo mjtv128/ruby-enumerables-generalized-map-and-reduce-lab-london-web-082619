0ac0088a57727bfe7fd63a5a7fcc8d334511c02d
jocular-source-7570 <mjtvan@gmail.com>
2019-08-22 18:47:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,13 @@
 # My Code here....
-def map 
-  
+def map(array)
+  new = []
+  i = 0 
+  while i < source_array.length do
+    new.push(source_array[i] * -1)
+    i += 1 
+  end 
+  return new 
+end 
   
 def map_to_negativize(source_array)
   source_array.map { |n| n * -1 }
