1b53bbd646beea80415bc3ec427e08f673a92153
jocular-source-7570 <mjtvan@gmail.com>
2019-08-22 18:14:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,16 +1,36 @@
-# Your Code Here
-def my_own_map(source_array)
-  source_array.map{|x| x * -1}
+# My Code here....
+require 'pry'
+def map_to_negativize(source_array)
+  source_array.map { |n| n * -1 }
 end 
 
-def my_own_map(source_array)
+def map_to_no_change(source_array)
   source_array.map{|x| x}
 end 
 
-def my_own_map(source_array)
-  source_array.map{|x| x*2}
+def map_to_double(source_array)
+  source_array.map{ |x| x*2}
 end 
 
-def my_own_map(source_array)
-  source_array.map{|x| x **2}
+def map_to_square(source_array)
+  source_array.map{|x| x**2}
 end 
+
+def reduce_to_total(source_array, starting_point =0)
+    if !starting_point
+      source_array.reduce(source_array[0], :+)
+    else 
+    source_array.reduce(starting_point, :+)
+  end 
+end 
+
+
+def reduce_to_all_true(source_array)
+  source_array.each{|x| return false if !x}
+end 
+
+def reduce_to_any_true(source_array)
+  source_array.each{|x| return true if x}
+  return false
+end 
+
