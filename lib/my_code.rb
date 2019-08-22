0c2340a112e8bb3c6624b19905ec53a1d8d86602
jocular-source-7570 <mjtvan@gmail.com>
2019-08-22 19:02:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,23 +8,8 @@ def map(source_array)
   end 
   return new 
 end 
-  
-# def map_to_negativize(source_array)
-#   source_array.map { |n| n * -1 }
-# end 
 
-# def map_to_no_change(source_array)
-#   source_array.map{|x| x}
-# end 
-
-# def map_to_double(source_array)
-#   source_array.map{ |x| x*2}
-# end 
-
-# def map_to_square(source_array)
-#   source_array.map{|x| x**2}
-# end 
-
+def reduce(array, startg)
 # def reduce_to_total(source_array, starting_point =0)
 #     if !starting_point
 #       source_array.reduce(source_array[0], :+)
